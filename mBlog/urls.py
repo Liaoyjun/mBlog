687df21ff091ed71fadd996e369f2af78a29652f
liaoyjun <18916108830@163.com>
2018-09-20 13:03:06 +0800

    Use bootstrap frame to design the front end.

--- mBlog/urls.py
+++ mBlog/urls.py
@@ -14,13 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from mainsite.views import homepage
+from django.urls import path, re_path
+from mainsite.views import homepage, showpost
 
 
 urlpatterns = [
-    path('', homepage),
+
     path('admin/', admin.site.urls),
+    path('', homepage),
+    re_path(r'^post/(\w+)/$', showpost)
+
 ]
 
 
