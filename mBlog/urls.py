3ea0f818360d891e4fb5e8811228d7f912c0628c
root <root@raspberrypi>
2018-10-11 21:07:55 +0800

    Fix about page, add several artivles.

--- mBlog/urls.py
+++ mBlog/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
 
     path('admin/', admin.site.urls),
     path('', homepage),
-    path('about', showAboutPage),
+    path('about/', showAboutPage),
     re_path(r'^article/(\w+)/(\w+)/$', showArticle),
     # re_path(r'^post/(?P<classification>\w+)/(?P<aid>\w+)/$', showArticlesList3)
     re_path(r'^article/(\w+)/$', showArticlesList)
